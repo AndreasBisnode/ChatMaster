3911af8de82ae9ce9eb9a00ef44b97eb175ac46c
andgra <andreas.graslund@soliditet.se>
2015-01-14 16:08:53 +0100

    ny wrapper

---
+++ src/main/java/message/Text.java
@@ -0,0 +1,22 @@
+package message;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.io.Serializable;
+
+/**
+ * Created by andgra on 2015-01-14.
+ */
+public class Text implements Serializable{
+    String text;
+
+    @JsonCreator
+    public Text(String text){
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+}
