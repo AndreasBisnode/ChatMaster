a5d9b041a6e53008c4ec84031254405cbc0ee9ad
andgra <andreas.graslund@bisnode.com>
2015-01-23 14:22:28 +0100

    contents and Id

---
+++ src/main/java/messagebucket/message/ContentsAndID.java
@@ -0,0 +1,27 @@
+package messagebucket.message;
+
+/**
+ * Created by andgra on 2015-01-22git s
+ */
+public class ContentsAndId {
+    private final Id id;
+    private final Content content;
+
+    public ContentsAndId(String id, String text, Timestamp dateTime) {
+        this.id = new Id(id);
+        this.content = new Content(text, dateTime);
+    }
+
+    public Timestamp timestamp() {
+        return content.timestamp();
+    }
+
+    public Text text() {
+
+        return content.text();
+    }
+
+    public Id Id() {
+        return id;
+    }
+}
