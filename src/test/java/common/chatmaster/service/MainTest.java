b0e6bd462b013dfb2509164fd7a70015e441b1fe
andgra <andreasgraslund@gmail.com>
2015-06-23 22:05:04 +0200

    Fas 4

--- src/test/java/common/chatmaster/service/MainTest.java
+++ src/test/java/common/chatmaster/service/MainTest.java
@@ -1,4 +1,4 @@
-package common.chatmaster;
+package common.chatmaster.service;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import common.chatmaster.service.ChatAdminService;
@@ -6,12 +6,18 @@ import common.chatmaster.service.ChatService;
 import common.chatmaster.subject.Channel;
 import common.chatmaster.subject.SubjectFactory;
 import common.chatmaster.subject.User;
-import common.config.AppConfig;
 import common.message.ChatMessage;
+import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.stereotype.Controller;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import javax.annotation.PostConstruct;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -20,19 +26,26 @@ import static org.junit.Assert.assertEquals;
 /**
  * Created by andgra on 2015-02-05.
  */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "/config.xml"})
+
+@Controller
 public class MainTest {
+
+
+
+    @Autowired
+    private ChatAdminService chatAdminService;
+    @Autowired
+    private ChatService chatService;
+    @Autowired
+    private ObjectMapper mapper;
+
     @Test
     public void mainTest() throws IOException {
-        ApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class);
-        ChatAdminService chatAdminService = (ChatAdminService) ctx.getBean("chatAdminService");
-        ChatService chatService = (ChatService) ctx.getBean("chatService");
-        ObjectMapper mapper = (ObjectMapper) ctx.getBean("objectMapper");
-
-
         InputStream resourceAsStream;
         JsonNode originalJson;
-        
-        
         //Create users
         User knatte = SubjectFactory.createUser("knatte", "knatte@mail.com");
         User fnatte = SubjectFactory.createUser("fnatte", "fnatte@mail.com");
@@ -65,15 +78,15 @@ public class MainTest {
         resourceAsStream = getClass().getResourceAsStream("/mainTest/knatteToKalle.json");
         originalJson = mapper.readTree(resourceAsStream);
         ChatMessage knatteToKalle = mapper.readValue(originalJson.toString(), ChatMessage.class);
-        
+
         resourceAsStream = getClass().getResourceAsStream("/mainTest/kalleToAll.json");
         originalJson = mapper.readTree(resourceAsStream);
         ChatMessage kalleToAll = mapper.readValue(originalJson.toString(), ChatMessage.class);
-        
+
         resourceAsStream = getClass().getResourceAsStream("/mainTest/kalleToKnatteRespons.json");
         originalJson = mapper.readTree(resourceAsStream);
         ChatMessage kalleToKnatteRespons = mapper.readValue(originalJson.toString(), ChatMessage.class);
-        
+
         resourceAsStream = getClass().getResourceAsStream("/mainTest/fnatteToChildren.json");
         originalJson = mapper.readTree(resourceAsStream);
         ChatMessage fnatteToChildren = mapper.readValue(originalJson.toString(), ChatMessage.class);
@@ -84,14 +97,14 @@ public class MainTest {
         chatService.sendMessage(kalleToKnatteRespons);
         chatService.sendMessage(fnatteToChildren);
 
-        assertEquals(chatService.retrieveMessages(knatte).size(), 4);
-        assertEquals(chatService.retrieveMessages(all).size(), 1);
-        assertEquals(chatService.retrieveMessages(kalle).size(), 3);
-        assertEquals(chatService.retrieveMessages(fnatte).size(), 2);
+        Assert.assertEquals(chatService.retrieveMessages(knatte).size(), 4);
+        Assert.assertEquals(chatService.retrieveMessages(all).size(), 1);
+        Assert.assertEquals(chatService.retrieveMessages(kalle).size(), 3);
+        Assert.assertEquals(chatService.retrieveMessages(fnatte).size(), 2);
 
-        assertEquals(chatService.retrieveMessages(tjatte).size(), 2);
+        Assert.assertEquals(chatService.retrieveMessages(tjatte).size(), 2);
         chatAdminService.unsubscribeChannel(tjatte, all);
-        assertEquals(chatService.retrieveMessages(tjatte).size(), 1);
+        Assert.assertEquals(chatService.retrieveMessages(tjatte).size(), 1);
         
         
     }
